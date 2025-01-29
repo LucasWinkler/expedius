defc52d0e39ba2fa59564c4545d82d05890228d9
Lucas Winkler <lucaswinkler@gmail.com>
2025-01-29 14:51:00 -0500

    Add next public to vercel url env

--- src/env.ts
+++ src/env.ts
@@ -23,8 +23,8 @@ export const env = createEnv({
       .url()
       .min(1)
       .default(
-        process.env.VERCEL_URL
-          ? `https://${process.env.VERCEL_URL}`
+        process.env.NEXT_PUBLIC_VERCEL_URL
+          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
           : "http://localhost:3000",
       ),
   },
