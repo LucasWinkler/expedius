62d6d0f97f3af6dda5666f71e6c0f2a8b859c85c
Lucas Winkler <lucaswinkler@gmail.com>
2025-02-04 01:00:41 -0500

    refac: remove unnecessary dynamic import for HomeCta

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic";
 import { createMetadata } from "@/lib/metadata";
-import { HomeHero, HomeFeatures } from "@/components/home";
-import { HomeCtaSkeleton } from "@/components/home/HomeCtaSkeleton";
+import { HomeHero, HomeFeatures, HomeCta } from "@/components/home";
 import { CategoryCarouselsSkeleton } from "@/components/home/CategoryCarousel/CategoryCarouselsSkeleton";
 
 const CategoryCarousels = dynamic(
@@ -13,16 +12,6 @@ const CategoryCarousels = dynamic(
     ),
   {
     loading: CategoryCarouselsSkeleton,
-  },
-);
-
-const HomeCta = dynamic(
-  () =>
-    import("@/components/home/HomeCta").then((mod) => ({
-      default: mod.HomeCta,
-    })),
-  {
-    loading: HomeCtaSkeleton,
   },
 );
 
