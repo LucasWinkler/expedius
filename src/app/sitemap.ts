1db2a191704bd58a0919a786d67b8496e00b3e9e
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-19 11:54:12 -0400

    chore: add privacy page to sitemap and update frequencies

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,20 +5,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://www.expedius.app",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "yearly",
       priority: 1,
     },
     {
       url: "https://www.expedius.app/discover",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "yearly",
       priority: 0.9,
     },
     {
       url: "https://www.expedius.app/categories",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "yearly",
       priority: 0.8,
     },
+    {
+      url: "https://www.expedius.app/privacy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    // {
+    //   url: "https://www.expedius.app/terms",
+    //   lastModified: new Date(),
+    //   changeFrequency: "yearly",
+    //   priority: 0.5,
+    // },
   ];
 }
