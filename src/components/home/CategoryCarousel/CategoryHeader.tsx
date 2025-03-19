711f8865cd73d52468dab388ea5988443a138b49
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-19 12:35:58 -0400

    style: update category header icon style

--- src/components/home/CategoryCarousel/CategoryHeader.tsx
+++ src/components/home/CategoryCarousel/CategoryHeader.tsx
@@ -6,8 +6,10 @@ interface CategoryHeaderProps {
 }
 
 export const CategoryHeader = ({ title, icon: Icon }: CategoryHeaderProps) => (
-  <div className="mb-4 flex items-center gap-4">
-    <Icon className="size-8 text-primary" />
-    <h2 className="text-2xl font-semibold">{title}</h2>
+  <div className="mb-4 flex items-center gap-3">
+    <div className="inline-flex rounded-lg bg-primary/10 p-2 text-primary">
+      <Icon className="size-6" />
+    </div>
+    <h3 className="text-2xl font-semibold">{title}</h3>
   </div>
 );
