f3615d059e8d3656f50557979106aa5fc0c0ce83
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-21 22:01:24 -0400

    style: set search bar ring + border to blue on focus visible

--- src/components/ui/search-input.tsx
+++ src/components/ui/search-input.tsx
@@ -15,7 +15,7 @@ export const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
         <Input
           ref={ref}
           className={cn(
-            "w-full rounded-full border-muted-foreground/20 bg-white py-6 pl-12 pr-32 shadow-sm transition-all duration-300 ease-out hover:border-muted-foreground/30 hover:shadow-md focus:border-muted-foreground/40 focus:shadow-lg 2xl:py-7 2xl:pl-14 2xl:pr-36 2xl:text-lg",
+            "w-full rounded-full border-muted-foreground/20 bg-white py-6 pl-12 pr-32 shadow-sm transition-all duration-300 ease-out hover:border-muted-foreground/30 hover:shadow-md focus-visible:border-blue-500 focus-visible:shadow-lg focus-visible:ring-blue-500 2xl:py-7 2xl:pl-14 2xl:pr-36 2xl:text-lg",
             className,
           )}
           value={value}
