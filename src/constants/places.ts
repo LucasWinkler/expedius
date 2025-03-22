ef03e917156e241756a3bc0a70fc42145fb47b14
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-22 18:31:09 -0400

    feat: add place_features array for place details

---
+++ src/constants/places.ts
@@ -0,0 +1,43 @@
+export const PLACE_FEATURES = [
+  { key: "dineIn", label: "Dine-in" },
+  { key: "takeout", label: "Takeout" },
+  { key: "delivery", label: "Delivery" },
+  { key: "curbsidePickup", label: "Curbside Pickup" },
+  { key: "reservable", label: "Reservations" },
+
+  { key: "servesBreakfast", label: "Breakfast" },
+  { key: "servesLunch", label: "Lunch" },
+  { key: "servesDinner", label: "Dinner" },
+  { key: "servesBrunch", label: "Brunch" },
+  { key: "servesDessert", label: "Dessert" },
+  { key: "servesCoffee", label: "Coffee" },
+  { key: "servesVegetarianFood", label: "Vegetarian Options" },
+
+  { key: "servesBeer", label: "Beer" },
+  { key: "servesWine", label: "Wine" },
+
+  { key: "outdoorSeating", label: "Outdoor Seating" },
+  { key: "liveMusic", label: "Live Music" },
+  { key: "menuForChildren", label: "Kid's Menu" },
+  { key: "restroom", label: "Restrooms" },
+  { key: "goodForWatchingSports", label: "Good for Watching Sports" },
+
+  { key: "paymentOptions.acceptsCreditCards", label: "Accepts Credit Cards" },
+  { key: "paymentOptions.acceptsDebitCards", label: "Accepts Debit Cards" },
+  { key: "paymentOptions.acceptsCashOnly", label: "Cash Only" },
+  { key: "paymentOptions.acceptsNfc", label: "Accepts Contactless Payments" },
+
+  { key: "parkingOptions.freeParking", label: "Free Parking" },
+  { key: "parkingOptions.paidParking", label: "Paid Parking" },
+  { key: "parkingOptions.streetParking", label: "Street Parking" },
+  { key: "parkingOptions.valetParking", label: "Valet Parking" },
+
+  {
+    key: "accessibilityOptions.wheelchairAccessibleParking",
+    label: "Wheelchair Accessible Parking",
+  },
+  {
+    key: "accessibilityOptions.wheelchairAccessibleEntrance",
+    label: "Wheelchair Accessible Entrance",
+  },
+] as const;
